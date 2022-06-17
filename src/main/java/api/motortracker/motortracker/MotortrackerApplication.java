64cbf30af895bae24856da9ff360623b3049f001
edelgado-git <enriquedelgado806@gmail.com>
2022-06-17 15:32:56 -0400

    Added modifications to the OPENAPI config

--- src/main/java/api/motortracker/motortracker/MotortrackerApplication.java
+++ src/main/java/api/motortracker/motortracker/MotortrackerApplication.java
@@ -1,9 +1,12 @@
 package api.motortracker.motortracker;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "MotorTracker API", version = "1.0", description = "API to handle requests from the mobile and webapp motortracker applications"))
 public class MotortrackerApplication {
 
 	public static void main(String[] args) {
